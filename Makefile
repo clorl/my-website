99dfe7d8d4e3579f9a517da31ef43e3a72a200fc
Clorl <clorl@github.com>
2026-04-03 23:03:39 +0200

    Quartz sync: Apr 3, 2026, 11:03 PM

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: sync 
+
+sync:
+	npx quartz sync
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Website",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,16 +16,16 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "clemorl.fr",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "IBM Plex Sans",
+        body: "Inter",
+        code: "JetBrains Mono",
       },
       colors: {
         lightMode: {
